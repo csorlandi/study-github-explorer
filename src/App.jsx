75beb9bf112ab94615f67c6363edd15fc84f4d66
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-03-16 01:08:15 -0300

    concepts: Understand Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
   // throw new Error('Eita Giovana, o forninho caiu');
 
-  return <h1>Hello World</h1>
+  return <RepositoryList />
 }
