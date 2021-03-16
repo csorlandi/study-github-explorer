fe5ec7217bc6edec3fddf2559f0151267aa4d546
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-03-16 01:28:19 -0300

    concepts: Understand Component State

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
   // throw new Error('Eita Giovana, o forninho caiu');
 
-  return <RepositoryList />
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  )
 }
