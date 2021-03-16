3bf97be153c9ff3bc376c851926653b526c601fd
Claudio Orlandi <claudiosorlandi@gmail.com>
2021-03-16 01:41:50 -0300

    chore: Styling RepositoryList Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
-import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
-  // throw new Error('Eita Giovana, o forninho caiu');
-
-  return (
-    <>
-      <RepositoryList />
-      <Counter />
-    </>
-  )
+  return <RepositoryList />;
 }
